62b38829784653241683c0b521904962ece4fe97
Augusto Pontes <augustopontes010@gmail.com>
2025-12-30 20:48:12 -0300

    Mongoose

--- JS/node/aula14/src/controllers/homeController.js
+++ JS/node/aula14/src/controllers/homeController.js
@@ -1,8 +1,16 @@
+const HomeModel = require('../models/HomeModel');
+
+HomeModel.find({
+    titulo: 'Outra coisa qualquer',
+    descricao: 'Outra descrição'
+})
+    .then(dados => console.log(dados))
+    .catch(e => console.log(e));
+
 exports.paginaInicial = (req, res, next) => {
     res.render('index');
     return;
 };
-
 
 exports.trataPost = (req, res) => {
     res.send(req.body);
